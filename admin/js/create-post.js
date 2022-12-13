382b773ad9345fd7cf9b0537836ec3c01fb87c5e
Emilio Bielanski <emilio.bielanski@outlook.com>
2022-12-13 12:52:34 +0100

    mergeCommit

--- admin/js/create-post.js
+++ admin/js/create-post.js
@@ -2,17 +2,22 @@ let title = document.getElementById("createTitle")
 let author = document.getElementById("createAuthor")
 let textArea = document.getElementById("createText")
 let form = document.getElementById("createForm")
-let formDataObject = serializeForm(form);
+
 form.addEventListener("submit", async function(e){
     const form = e.target
-    await fetch("https://blog-api-assignment.up.railway.app/posts", {
+    e.preventDefault();
+   try{ await fetch("https://blog-api-assignment.up.railway.app/posts", {
         method:"POST",
         headers: {
+            'Accept': 'application/json',
             'Content-Type': 'application/json'
           },
           body: JSON.stringify(formDataObject)
     })
-    
+    }catch(error){
+        console.log(error)
+    }
+    console.log("test")
 })
 
 
@@ -35,3 +40,4 @@ let serializeForm = function (form) {
     // console.log(obj);
     return obj;
 };
+let formDataObject = serializeForm(form);

1ec003a710ce429e845f9141f9c276fd6daf5c64
Emilio Bielanski <emilio.bielanski@outlook.com>
2022-12-13 12:12:16 +0100

    createHTMLdone

--- admin/js/create-post.js
+++ admin/js/create-post.js
@@ -1,5 +1,37 @@
-let title = document.getElementById
-let author
-let textArea
-let form
-let submitbtn
+let title = document.getElementById("createTitle")
+let author = document.getElementById("createAuthor")
+let textArea = document.getElementById("createText")
+let form = document.getElementById("createForm")
+let formDataObject = serializeForm(form);
+form.addEventListener("submit", async function(e){
+    const form = e.target
+    await fetch("https://blog-api-assignment.up.railway.app/posts", {
+        method:"POST",
+        headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formDataObject)
+    })
+    
+})
+
+
+
+let serializeForm = function (form) {
+    var obj = {};
+    var formData = new FormData(form);
+    // console.log(formData.getAll());
+
+    for (var key of formData.keys()) {
+        let inputData = formData.getAll(key);
+
+        if (inputData.length > 1) {
+            obj[key] = inputData;
+        } else {
+            obj[key] = inputData[0];    
+        }
+    }
+    
+    // console.log(obj);
+    return obj;
+};

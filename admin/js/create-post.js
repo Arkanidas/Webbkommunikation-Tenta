569624914865c2cebad65051a6b2cc2755975674
Emilio Bielanski <emilio.bielanski@outlook.com>
2022-12-15 15:58:25 +0100

    addedMilosThings Done

--- admin/js/create-post.js
+++ admin/js/create-post.js
@@ -1,43 +1,61 @@
 let title = document.getElementById("createTitle")
 let author = document.getElementById("createAuthor")
 let textArea = document.getElementById("createText")
-let form = document.getElementById("createForm")
+let createForm = document.getElementById("createForm")
 
-form.addEventListener("submit", async function(e){
+
+
+createForm.addEventListener('submit', async function (e) {
+    e.preventDefault()
     const form = e.target
-    e.preventDefault();
-   try{ await fetch("https://blog-api-assignment.up.railway.app/posts", {
-        method:"POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(formDataObject)
-    })
-    }catch(error){
-        console.log(error)
-    }
-    console.log("test")
-})
 
+    let serializeForm = function (form) {
+  
+      var obj = {};
+      var formData = new FormData(form);
+  
+  
+      for (var key of formData.keys()) {
+          let inputData = formData.getAll(key);
+  
+          if (inputData.length > 1) {
+              obj[key] = inputData;
+          } else {
+              obj[key] = inputData[0];
+          }
+      }
+  
+      return obj;
+  };
+  let formDataObjekt = serializeForm(form);
+  
+      let titles = title
+      let authors = author
+      let texts = textArea
+      let selection1 = document.getElementById('tags')
+  
+    const blogpost = {
+  
+          title: titles.value,
+          author: authors.value,
+          content: texts.value,
+         tags: selection1.value
+  
+      }
+  
+      try {
+          await fetch('https://blog-api-assignment.up.railway.app/posts', {
+              method: 'POST', 
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(blogpost),
+  
+          })
+  
+         location.replace('index.html')
+      } catch (error) {
+          console.log(error)
+      }
+  })
 
-
-let serializeForm = function (form) {
-    var obj = {};
-    var formData = new FormData(form);
-    // console.log(formData.getAll());
-
-    for (var key of formData.keys()) {
-        let inputData = formData.getAll(key);
-
-        if (inputData.length > 1) {
-            obj[key] = inputData;
-        } else {
-            obj[key] = inputData[0];    
-        }
-    }
-    
-    // console.log(obj);
-    return obj;
-};
-let formDataObject = serializeForm(form);
--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -33,3 +33,4 @@ async function getBlogPosts(){
 }
 getBlogPosts();
 
+
